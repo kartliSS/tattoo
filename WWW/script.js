808e859193e8c706d6e79411b2edcf777a16ea16
kartliSS <50672101+kartliSS@users.noreply.github.com>
2019-05-16 14:40:54 +0300

    Файлы для сайта

---
+++ WWW/script.js
@@ -0,0 +1,22 @@
+/*menu*/
+menu.onclick = function myFunction() {
+	var x = document.getElementById('myTopnav');
+
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
+
+
+/*slider*/
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
+
+function nextSlide(){
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
